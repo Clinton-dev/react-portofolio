b43ac76084169813f24418a19a6b3a00d5897ff3
Clinton wambugu <clintonwambugu@gmail.com>
2023-01-22 18:35:15 +0300

    Add github and live icons

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import WorkItem from "./WorkItem";
+import projects from '../projects'
 
-WorkItem
 
 function Work() {
+  const myWorks = projects.map((project, index) => <WorkItem key={index} project={project}/>)
+
   return (
     <div id="work">
      <h3>My Work</h3>
+     {myWorks}
     </div>
     );
 }
--- src/projects.js
+++ src/projects.js
@@ -3,7 +3,7 @@ export default [
     title: "Static Job Listing",
     summary:
       " A job listing board Where users can filter jobs based on various programming languages built with Reactjs, tailwind and vite",
-    img: "",
+    img: "/static-job-listing.jpg",
     github: "https://github.com/Clinton-dev/static-job-listing",
     live: "https://jocular-muffin-d57f50.netlify.app/",
     completed: true,
@@ -12,16 +12,16 @@ export default [
     title: "Time Tracker Dash",
     summary:
       "A dashboard that aids in tracking how much time you spend on various activites",
-    img: "",
+    img: "/time-tracker-dash.jpg",
     github: "https://github.com/Clinton-dev/time-tracker-dash",
-    live: "TimeTrackerDash",
+    live: "https://polite-florentine-296e09.netlify.app/",
     completed: true,
   },
   {
     title: "Moringa stack",
     summary:
       "Moringa-Stack is a community web application for Moringa students where they can tackle similar problems they encounter during their study period and also when doing their Independent Projects. Its built with django and custom css",
-    img: "",
+    img: "/defaultproject.png",
     github: "https://github.com/Clinton-dev/Moringa-Stack",
     live: "https://moringastack.herokuapp.com/",
     completed: true,
@@ -29,8 +29,8 @@ export default [
   {
     title: "Rotary Blood Bank",
     summary:
-      "A web app for blood groups where users in need of a specific blood group can register their details, log in to a system where the doctor admins can screen the blood and link the people in need of the specified blood group to correspondent donors. Give BLOOD save LIVES. ",
-    img: "",
+      "A web app for blood groups where users in need of a specific blood group can register their details, log in to a system where the doctor admins can screen the blood and link the people in need of the specified blood group to correspondent donors. ",
+    img: "/defaultproject.png",
     github: "https://github.com/Clinton-dev/Rotary-Blood-Bank",
     live: "https://salty-bastion-21477.herokuapp.com/",
     completed: true,
@@ -39,7 +39,7 @@ export default [
     title: "Space tour App",
     summary:
       "An app that shows you various details about space destinations, space flight and NASA",
-    img: "",
+    img: "/space-tour-app.jpg",
     github: "https://github.com/Clinton-dev/space-tourism-website",
     live: "",
     completed: false,
