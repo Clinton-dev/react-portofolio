3b09ba925d8f02a39bbf29fac64b0f6510e3f08f
Clinton wambugu <clintonwambugu@gmail.com>
2023-01-27 14:19:26 +0300

    Change display layout of work items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,19 +36,18 @@ function Footer() {
         <div id='contact-section'>
           <h4>Say Hi!</h4>
           <p>Currently open for new opportunities or people to share ideas with! For any other information about me or my work, feel free to reach out!</p>
+          <button>View resume</button>
           <div id='contact-details'>
             <p>Contact me at clintonwambugu@gmail.com</p>
-            <p>
-              Or find me here <br/>
-              <div style={{display: 'flex'}}>
-                <a href=" www.linkedin.com/in/clintonwambugu">
+            <p>Or find me here: </p><br/>
+              <div  style={{display: 'flex'}}>
+                <a className='social-links' href=" www.linkedin.com/in/clintonwambugu">
                   <img src={linkedin}/>
                 </a>
-                <a href="https://twitter.com/WambuguClinton">
+                <a className='social-links' href="https://twitter.com/WambuguClinton">
                   <img src={twitter}/>
                 </a>
               </div>
-            </p>
           </div>
         </div>
         <div id='form-section'>
@@ -59,7 +58,7 @@ function Footer() {
               <input type="email" name="email" id="email" placeholder='email' value={email} onChange={handleEmail}/>
             </div>
             <input type="text" name='subject' id='subject' placeholder='subject' value={subject} onChange={handleSubject}/>
-            <textarea name="message" id="message" cols="30" rows="5" value={message} onChange={handleMessage}>
+            <textarea name="message" id="message" rows="5" value={message} onChange={handleMessage}>
               Your message
             </textarea>
             <button>Send Message</button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,28 @@ import menuUrl from '../assets/menu-3-line.svg'
 function Navbar() {
   return (
     <nav>
-      <div></div>
+      {/* mobile nav */}
+      <div className="mobile-nav">
+        <div></div>
       <button>
         <img src={menuUrl}/>
       </button>
+      </div>
+
+      {/* desktop and tablet nav */}
+      <div className="desktop-nav">
+        <ul>
+          <li>
+            <a href="#header">About</a>
+          </li>
+          <li>
+            <a href="#work">Work</a>
+          </li>
+          <li>
+            <a href="#contact-section">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
